ee8f94f5097e48096c31347db7385346f9e0e42e
jaden <jaden210@gmail.com>
2017-04-03 23:14:19 -0600

    service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Http } from '@angular/http';
 import { AppService } from './app.service';
 
 @Component({
@@ -11,40 +10,14 @@ import { AppService } from './app.service';
   ]
 })
 export class AppComponent implements OnInit {
-
-  schoolId: any;
-  schools = [
-    { id: 1, name: "Desert Hills", zipCode: "84780" },
-    { id: 2, name: "Desert Hills", zipCode: "84781" },
-    { id: 3, name: "Desert Hills", zipCode: "84782" },
-  ];
+  
   toolbarOn: boolean;
 
-  constructor(private _http: Http, private _appService: AppService) { }
+  constructor(private _appService: AppService) { }
 
   ngOnInit() {
-    if (localStorage.getItem("schoolId")) {
-      this.schoolId = localStorage.getItem("schoolId")
-    } else {
-      window.navigator.geolocation.getCurrentPosition((pos) => {
-        return this._http.get('http://maps.googleapis.com/maps/api/geocode/json?latlng=' + pos.coords.latitude + ',' + pos.coords.longitude + '&sensor=true')
-          .toPromise()
-          .then((res) => {
-            this.selectSchool(res.json().results[0].address_components[6].long_name.toString());
-          });
-      });
-    };
+    this._appService.localStorageLookup();
     this.checkPosition()
-  }
-
-  selectSchool(zipCode) {
-    for (let school of this.schools) {
-      if (school.zipCode.includes(zipCode)) {
-        localStorage.setItem("schoolId", school.id.toString());
-        this.schoolId = school.id;
-        return;
-      }
-    }
   }
 
   checkPosition(): void {
--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,8 +1,40 @@
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Injectable()
 export class AppService {
 
-  constructor() { }
+  schoolId: any;
+  schools = [
+    { id: 1, name: "Desert Hills", zipCode: "84780" },
+    { id: 2, name: "Desert Hills", zipCode: "84781" },
+    { id: 3, name: "Desert Hills", zipCode: "84782" },
+  ];
+
+  constructor(private _http: Http,) { }
+
+    localStorageLookup() {
+      if(localStorage.getItem("schoolId")) {
+        this.schoolId = localStorage.getItem("schoolId")
+      } else {
+        window.navigator.geolocation.getCurrentPosition((pos) => {
+          return this._http.get('http://maps.googleapis.com/maps/api/geocode/json?latlng=' + pos.coords.latitude + ',' + pos.coords.longitude + '&sensor=true')
+            .toPromise()
+            .then((res) => {
+              this.selectSchool(res.json().results[0].address_components[6].long_name.toString());
+            });
+        });
+      };
+    }
+
+    selectSchool(zipCode) {
+    for (let school of this.schools) {
+      if (school.zipCode.includes(zipCode)) {
+        localStorage.setItem("schoolId", school.id.toString());
+        this.schoolId = school.id;
+        return;
+      }
+    }
+  }
 
 }
